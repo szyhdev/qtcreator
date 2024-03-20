413d811e0da024afb78c63a3f00249a0291b1fea
szyhdev <szyuanhua@163.com>
2024-03-20 16:04:53 +0800

    update project 02_01

--- 02_hello_world/02_01/helloworld/hellodialog.cpp
+++ 02_hello_world/02_01/helloworld/hellodialog.cpp
@@ -1,8 +1,8 @@
 #include "hellodialog.h"
 #include "ui_hellodialog.h"
 
-HelloDialog::HelloDialog(QWidget *parent)
-    : QDialog(parent), ui(new Ui::HelloDialog)
+HelloDialog::HelloDialog(QWidget *parent) :
+        QDialog(parent), ui(new Ui::HelloDialog)
 {
     ui->setupUi(this);
 }
