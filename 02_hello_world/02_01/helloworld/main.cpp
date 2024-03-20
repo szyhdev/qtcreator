98f6e02594084a4afc30d60b7bcd9718eac5965e
szyhdev <szyuanhua@163.com>
2024-03-20 15:08:20 +0800

    add project 02_01

---
+++ 02_hello_world/02_01/helloworld/main.cpp
@@ -0,0 +1,13 @@
+#include "hellodialog.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    HelloDialog w;
+    w.show();
+
+    return a.exec();
+}
