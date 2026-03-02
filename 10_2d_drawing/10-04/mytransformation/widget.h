1d4a54b73667c16eec390758e41ef2fcf49aa4c8
rhd <szyuanhua@163.com>
2026-03-02 22:25:34 +0800

    add project 10_04

---
+++ 10_2d_drawing/10-04/mytransformation/widget.h
@@ -0,0 +1,29 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+class QTimer;
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private:
+    Ui::Widget *ui;
+    QTimer *timer;
+    int angle;
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+};
+
+#endif // WIDGET_H
