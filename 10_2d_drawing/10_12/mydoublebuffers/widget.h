37ddd08686a5d6dcd2ab0a08dcd77464865be4e7
rhd <szyuanhua@163.com>
2026-03-03 16:09:37 +0800

    add project 10_12

---
+++ 10_2d_drawing/10_12/mydoublebuffers/widget.h
@@ -0,0 +1,34 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private:
+    Ui::Widget *ui;
+
+    QPixmap pix;
+    QPixmap tempPix;
+    QPoint startPoint;
+    QPoint endPoint;
+    bool isDrawing;
+
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    void paintEvent(QPaintEvent *event) override;
+};
+
+#endif // WIDGET_H
